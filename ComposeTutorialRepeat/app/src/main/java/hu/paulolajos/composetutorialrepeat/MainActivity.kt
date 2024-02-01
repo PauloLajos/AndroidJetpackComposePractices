015ecf841b252a673e5f5679daacce478f4a5739
PauloLajos <paulolajos@gmail.com>
2024-02-01 06:47:04 +0100

    Lesson 3: Material Design - Use Material Design

--- ComposeTutorialRepeat/app/src/main/java/hu/paulolajos/composetutorialrepeat/MainActivity.kt
+++ ComposeTutorialRepeat/app/src/main/java/hu/paulolajos/composetutorialrepeat/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,12 +21,19 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import hu.paulolajos.composetutorialrepeat.ui.theme.ComposeTutorialRepeatTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            ComposeTutorialRepeatTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(
+                        Message("Android", "Jetpack Compose")
+                    )
+                }
+            }
         }
     }
 }
@@ -63,10 +72,11 @@ fun MessageCard(msg: Message) {
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message(
-            "Lexi",
-            "Hey, take a look at Jetpack Compose, it's great!"
-        )
-    )
+    ComposeTutorialRepeatTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Lexi", "Take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }
 }
