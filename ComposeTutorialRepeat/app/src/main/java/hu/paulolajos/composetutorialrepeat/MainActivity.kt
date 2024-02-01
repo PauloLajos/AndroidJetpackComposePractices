cde283a6c3a1a4966edbfe42db831856abf867f5
PauloLajos <paulolajos@gmail.com>
2024-02-01 06:57:05 +0100

    Lesson 3: Material Design - Enable dark theme

--- ComposeTutorialRepeat/app/src/main/java/hu/paulolajos/composetutorialrepeat/MainActivity.kt
+++ ComposeTutorialRepeat/app/src/main/java/hu/paulolajos/composetutorialrepeat/MainActivity.kt
@@ -1,5 +1,6 @@
 package hu.paulolajos.composetutorialrepeat
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -91,7 +92,12 @@ fun MessageCard(msg: Message) {
     }
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
     ComposeTutorialRepeatTheme {
