bcbefc84151657e1f53506f352abc07bf7ad1bdf
PauloLajos <paulolajos@gmail.com>
2024-02-02 05:44:17 +0100

    Start new project: Persist data with Room and Compose

---
+++ PersistdatawithRoomandCompose/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Persist data with Room and Compose"
+include(":app")
